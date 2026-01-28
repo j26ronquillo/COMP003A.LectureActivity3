f21cbc20602d0b7103c6132fd42015b3a1be4ba6
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-27 18:13:32 -0800

    created section headers

---
+++ COMP003A.LectureActivity3/COMP003A.LectureActivity3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace COMP003A.LectureActivity3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SECTION A — Academic Term Identification");
+
+            Console.WriteLine("SECTION B — Campus Access Check");
+
+            Console.WriteLine("SECTION C — Semester Code Lookup");
+		}
+    }
+}
