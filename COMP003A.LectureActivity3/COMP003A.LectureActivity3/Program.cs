8bc1b73265bfa1482cddc50f62d58fd5bd917859
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-27 18:31:26 -0800

    create the if/else if/else statements for semester values

--- COMP003A.LectureActivity3/COMP003A.LectureActivity3/Program.cs
+++ COMP003A.LectureActivity3/COMP003A.LectureActivity3/Program.cs
@@ -9,6 +9,23 @@
             Console.WriteLine("SECTION B — Campus Access Check");
 
             Console.WriteLine("SECTION C — Semester Code Lookup");
+
+            int currentMonth = DateTime.Now.Month;
+
+			Console.Write($"Month {currentMonth}: ");
+
+            if (currentMonth >= 1 && currentMonth <= 5)
+            {
+                Console.WriteLine("Spring Term");
+            }
+            else if (currentMonth == 6 || currentMonth == 7)
+            {
+                Console.WriteLine("Summer Term");
+            }
+            else
+            {
+                Console.WriteLine("Fall Term");
+            }
 		}
     }
 }
