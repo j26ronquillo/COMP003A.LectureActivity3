2285afef0f6c37866d6a939e8aed1275abcfed6d
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-03 21:59:22 -0800

    finished answering the comment questions (must find a way to output 1 for the loop control section)

--- COMP003A.LectureActivity4/COMP003A.LectureActivity4/Program.cs
+++ COMP003A.LectureActivity4/COMP003A.LectureActivity4/Program.cs
@@ -28,6 +28,29 @@ namespace COMP003A.LectureActivity4
             int stepNumber = 1;
             for (stepNumber = 1;  stepNumber <= 4; stepNumber++)
             { Console.WriteLine($"Step {stepNumber} "); }
+
+            int number = 1;
+                while ( number <= 6)
+            {
+                number++;
+                 if (number == 4)
+                { //continue is used to skip an iteration and move on to the next number/step
+                    continue;
+                }
+                 if (number == 6)
+                { // break is used to stop/terminate a loop even if the condition is still true
+                    break;
+                }
+                Console.WriteLine($"{number}");
+            }
+                // 1. The for loop was easiest to understand because I was able to create all the
+                // and laws in a single line, making it easier to understand.
+
+                // 2. The while loop probably requires the most careful tracing because you can accidently
+                // create an infinite loop if not made an exit line.
+
+                // 3. The comments helped me understand the different loops because it show me the specific
+                // lines that created, terminated, and evaluated in the loop process.
         }
     }
 }
