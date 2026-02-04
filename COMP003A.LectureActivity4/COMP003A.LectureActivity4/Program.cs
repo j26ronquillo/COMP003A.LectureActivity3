34249e2cbed4413caaeb533f52b5e1132d3aaecb
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-03 21:35:12 -0800

    created the "do-while" and "for" loops

--- COMP003A.LectureActivity4/COMP003A.LectureActivity4/Program.cs
+++ COMP003A.LectureActivity4/COMP003A.LectureActivity4/Program.cs
@@ -12,6 +12,22 @@ namespace COMP003A.LectureActivity4
             { Console.WriteLine($"Task {taskNumber} completed");
                 taskNumber++; 
             }
+           
+            //this loop executes atleast once because the condition is checked
+            //at the end of the loop, so the code inside the block runs before
+            //the actual loop condition is evaluated and ultimately continues
+            int attempt = 1;
+            do
+            {
+                Console.WriteLine($"Attempt {attempt}");
+                attempt++;
+            }
+            while ( attempt <= 3);
+
+            //the for loop is used here because the number of reps is set and known in advance
+            int stepNumber = 1;
+            for (stepNumber = 1;  stepNumber <= 4; stepNumber++)
+            { Console.WriteLine($"Step {stepNumber} "); }
         }
     }
 }
